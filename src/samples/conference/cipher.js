56580a61ec38157729cb394580b406cc6e93414f
Jianjun Zhu <jianjun.zhu@intel.com>
2018-04-16 12:56:48 +0800

    Fix conference sample.
    Adding missing cipher.js and remove test.html.
    
    Change-Id: I3b8d3cd1a9d45dac2d2783586d78761fde5d2efc
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/170226
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>

---
+++ src/samples/conference/cipher.js
@@ -0,0 +1,65 @@
+/* global require, module */
+'use strict';
+
+var crypto = require('crypto');
+var algorithm = 'aes-256-ctr';
+var stream = require('stream');
+var fs = require('fs');
+var zlib = require('zlib');
+
+function encrypt(password, text) {
+  var cipher = crypto.createCipher(algorithm, password);
+  var enc = cipher.update(text, 'utf8', 'hex');
+  enc += cipher.final('hex');
+  return enc;
+}
+
+function decrypt(password, text) {
+  var decipher = crypto.createDecipher(algorithm, password);
+  var dec = decipher.update(text, 'hex', 'utf8');
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+function lock(password, object, filename, cb) {
+  var s = new stream.Readable();
+  s._read = function noop() {};
+  s.push(JSON.stringify(object));
+  s.push(null);
+  var out = fs.createWriteStream(filename);
+  out.on('error', function(e) {
+    cb(e);
+  });
+  out.on('finish', function() {
+    cb(null);
+  });
+  s.pipe(zlib.createGzip()).pipe(crypto.createCipher(algorithm, password)).pipe(
+    out);
+}
+
+function unlock(password, filename, cb) {
+  var s = fs.createReadStream(filename);
+  s.on('error', function(e) {
+    cb(e);
+  });
+  var unzip = zlib.createGunzip();
+  var buf = '';
+  unzip.on('data', function(chunk) {
+    buf += chunk.toString();
+  });
+  unzip.on('end', function() {
+    cb(null, JSON.parse(buf));
+  });
+  unzip.on('error', function(e) {
+    cb(e);
+  });
+  s.pipe(crypto.createDecipher(algorithm, password)).pipe(unzip);
+}
+
+module.exports = {
+  encrypt: encrypt,
+  decrypt: decrypt,
+  k: crypto.pbkdf2Sync('woogeen', 'mcu', 4000, 128, 'sha1'),
+  lock: lock,
+  unlock: unlock
+};
