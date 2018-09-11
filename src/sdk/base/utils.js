b74cfe59c3f53ba49a3c0ba4036cb30f3c6569d6
Zhang He <he.zhang@intel.com>
2018-09-11 09:59:05 +0800

    Correct user agent misspelling.
    Change-Id: I33521116257a7d47a719dee235534ca2b9cb7932
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/189589
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- src/sdk/base/utils.js
+++ src/sdk/base/utils.js
@@ -105,7 +105,7 @@ export function sysInfo() {
     };
   }
   info.capabilities = {
-    iceContinualGathering: false,
+    continualIceGathering: false,
     unifiedPlan: false,
     streamRemovable: info.runtime.name !== 'Firefox'
   };
