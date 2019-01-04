6968b1b2fdb850026c13303198af467c1e4a114c
zhangyihui1 <42208024+zhangyihui1@users.noreply.github.com>
2019-01-04 13:43:39 +0800

    Download dependenies with python script. (#64)

---
+++ test/p2ptest/fetch_dependencies.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+
+try:
+  import urllib2 as u
+except:
+  import urllib.request as u
+
+
+URLS = {'q.js': 'https://raw.githubusercontent.com/kriskowal/q/c2f5a6f35456389a806acca50bfd929cbe30c4cb/q.js',
+        'jquery.min.js': 'https://code.jquery.com/jquery-1.10.2.min.js',
+        'socket.io.min.js': 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min.js',
+        'adapter.js': 'https://webrtc.github.io/adapter/adapter-7.0.0.js'
+        }
+
+DEPS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'dependencies'))
+
+
+def download(file_name, url):
+  req = u.Request(url)
+  res_data = u.urlopen(req)
+  with open(os.path.join(DEPS_PATH, file_name), 'wb') as f:
+    f.write(res_data.read())
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Download some files into ./dependencies')
+  parser.parse_args()
+  if not os.path.exists(DEPS_PATH):
+    os.makedirs(DEPS_PATH)
+  for name, url in URLS.items():
+    download(name, url)
