bd722957777056e5b5021cda4776f311e6ac64b7
Chen Li1 <li1.chen@intel.com>
2019-04-01 13:16:27 +0800

    Change default option to HTTPS in sample

--- src/samples/conference/samplertcservice.js
+++ src/samples/conference/samplertcservice.js
@@ -63,7 +63,7 @@ app.use(function(req, res, next) {
   }
 });
 
-icsREST.API.init('_service_ID_', '_service_KEY_', 'http://localhost:3000/', true);
+icsREST.API.init('_service_ID_', '_service_KEY_', 'https://localhost:3000/', false);
 
 var sampleRoom;
 var pageOption = { page: 1, per_page: 100 };
