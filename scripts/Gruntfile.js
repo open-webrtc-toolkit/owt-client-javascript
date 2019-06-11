5d704a0eea41819cfbc573cf5e574679a9487b83
Geoff Gustafson <geoff@linux.intel.com>
2019-06-11 15:19:53 -0700

    Add ESLint check task to grunt and add package deps
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- scripts/Gruntfile.js
+++ scripts/Gruntfile.js
@@ -257,6 +257,7 @@ window.L = L;\n\
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-jsdoc');
 
+  grunt.registerTask('check', ['eslint:src']);
   grunt.registerTask('prepare', ['browserify:sinon', 'browserify:chai_as_promised']);
   grunt.registerTask('pack', ['browserify:dist', 'concat:rest', 'uglify:dist', 'copy:dist', 'string-replace', 'compress:dist', 'jsdoc:dist']);
   grunt.registerTask('dev', ['browserify:dev', 'connect:server']);
