cf7c7f21c89f47edea367c77b7be7fb27840f81c
zhangyihui1 <yihuizhx@intel.com>
2019-07-09 15:35:37 +0800

    Test p2p js with chrome headless mode.

--- test/p2ptest/karma_config/chrome-start.sh
+++ test/p2ptest/karma_config/chrome-start.sh
@@ -17,4 +17,5 @@ fi
 
 # Execute the command
 export DISPLAY=:0
-exec "$CMD" --user-data-dir="$USER_DIR" --proxy-auto-detect --window-size=950,450 --use-fake-ui-for-media-stream --disable-user-media-security --no-default-browser-check --no-first-run --disable-default-apps --use-fake-device-for-media-stream  "$@"
+exec "$CMD" --user-data-dir="$USER_DIR" --enable-logging --v=1 --allow-file-access-from-files  --no-proxy-server --no-sandbox --use-fake-ui-for-media-stream --disable-user-media-security --no-default-browser-check --no-first-run --disable-default-apps --disable-popup-blocking  --use-fake-device-for-media-stream --headless --remote-debugging-port=9222 "$@"
+#exec "$CMD" --user-data-dir="$USER_DIR" --proxy-auto-detect --window-size=950,450 --use-fake-ui-for-media-stream --disable-user-media-security --no-default-browser-check --no-first-run --disable-default-apps --use-fake-device-for-media-stream  "$@"
